3f00c6d1fce4a83ac98418c137c1a8b7325ae646
Bruno BELANYI <bruno@belanyi.fr>
2022-07-22 18:41:09 +0200

    Make 'Magic' fields 'pub(crate)'

--- src/movegen/magic/magic.rs
+++ src/movegen/magic/magic.rs
@@ -4,13 +4,13 @@ use crate::board::Bitboard;
 #[allow(unused)] // FIXME: remove once used
 pub struct Magic {
     /// Magic number.
-    magic: u64,
+    pub(crate) magic: u64,
     /// Base offset into the magic square table.
-    offset: usize,
+    pub(crate) offset: usize,
     /// Mask to apply to the blocker board before applying the magic.
-    mask: Bitboard,
+    pub(crate) mask: Bitboard,
     /// Length of the resulting mask after applying the magic.
-    shift: u8,
+    pub(crate) shift: u8,
 }
 
 impl Magic {
