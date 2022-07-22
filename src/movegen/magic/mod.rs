5ef3737b988a7436ac9fd165fe74fa15c54c8c62
Bruno BELANYI <bruno@belanyi.fr>
2022-07-22 18:41:09 +0200

    Make 'Magic' fields 'pub(crate)'

--- src/movegen/magic/mod.rs
+++ src/movegen/magic/mod.rs
@@ -3,13 +3,13 @@ use crate::board::Bitboard;
 /// A type representing the magic board indexing a given [crate::board::Square].
 pub struct Magic {
     /// Magic number.
-    magic: u64,
+    pub(crate) magic: u64,
     /// Base offset into the magic square table.
-    offset: usize,
+    pub(crate) offset: usize,
     /// Mask to apply to the blocker board before applying the magic.
-    mask: Bitboard,
+    pub(crate) mask: Bitboard,
     /// Length of the resulting mask after applying the magic.
-    shift: u8,
+    pub(crate) shift: u8,
 }
 
 impl Magic {
