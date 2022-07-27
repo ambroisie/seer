bd662fdd27b7f363458c765b8447883051e45e7a
Bruno BELANYI <bruno@belanyi.fr>
2022-07-27 23:24:24 +0200

    Introduce 'FenError' enum

--- src/fen.rs
+++ src/fen.rs
@@ -4,3 +4,21 @@ pub trait FromFen: Sized {
 
     fn from_fen(s: &str) -> Result<Self, Self::Err>;
 }
+
+/// A singular type for all errors that could happen during FEN parsing.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum FenError {
+    /// Invalid FEN input.
+    InvalidFen,
+}
+
+impl std::fmt::Display for FenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let error_msg = match self {
+            Self::InvalidFen => "Invalid FEN input",
+        };
+        write!(f, "{}", error_msg)
+    }
+}
+
+impl std::error::Error for FenError {}
