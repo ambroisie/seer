08ff8db0ac5cec2909841ab61341f647e15318c2
Bruno BELANYI <bruno@belanyi.fr>
2022-07-28 21:17:12 +0200

    Add 'Error::InvalidPosition' variant

--- src/error.rs
+++ src/error.rs
@@ -2,12 +2,14 @@
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Error {
     InvalidFen,
+    InvalidPosition,
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let error_msg = match self {
             Self::InvalidFen => "Invalid FEN input",
+            Self::InvalidPosition => "Invalid position",
         };
         write!(f, "{}", error_msg)
     }
